1155d0fb0a047f333153ffa574be00a5cb837692
Andrew Boyley <andy.boyley@gmail.com>
2026-06-08 18:30:42 +0200

    Removed api from opencode cliproxyapi

--- dot_config/hypr/custom/general.lua
+++ dot_config/hypr/custom/general.lua
@@ -1,37 +1,10 @@
 
+
 hl.gesture({
     fingers = 4,
-    direction = "right",
-    mod = "Alt",
-    action = "dispatcher",
-    arg = "movetoworkspacesilent, r+1"
+    action = "workspace"
 })
-hl.gesture({
-    fingers = 4,
-    direction = "left",
-    mod = "Alt",
-    action = "dispatcher",
-    arg = "movetoworkspacesilent, r-1"
-})
-hl.gesture({
-    fingers = 4,
-    direction = "left",
-    mod = "Shift",
-    action = "dispatcher",
-    arg = "movetoworkspace, r+1"
-})
-hl.gesture({
-    fingers = 4,
-    direction = "right",
-    mod = "Shift",
-    action = "dispatcher",
-    arg = "movetoworkspace, r-1"
-})
-hl.gesture({
-    fingers = 4,
-    direction = "down",
-    action = "unset"
-})
+
 hl.gesture({
     fingers = 4,
     direction = "down",
